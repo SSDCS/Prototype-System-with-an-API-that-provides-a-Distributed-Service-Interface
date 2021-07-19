07a5a62551076d6fe9e358aacf750df2f2f32723
anrichp <anrichp@gmail.com>
2021-07-19 20:52:07 +0100

    update static and templates diretory

--- dashboard/application/dashboard/__init__.py
+++ dashboard/application/dashboard/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('dashboard', __name__, static_folder="static", template_folder="templates")
+bp = Blueprint('dashboard', __name__, static_folder="./static", template_folder="./templates")
 
 from . import routes
